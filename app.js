b1622f14f05b543c7f10c6ac65887bf3b00f435d
Bostind <Bostind@msn.com>
2025-06-17 17:16:15 +0800

    修改端口

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { generateCalendar } = require('./utils/calendar');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6067;
 
 // Python 后端服务的URL
 const PYTHON_BACKEND_URL = process.env.PYTHON_BACKEND_URL || 'http://localhost:5000';
